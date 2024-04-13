bea765038033bd7eeceeba57c9f6410c6bf1980d
ElianeRiveraCastillo22 <elianercast@gmail.com>
2024-04-13 10:38:44 -0500

    feat: level up

--- game.js
+++ game.js
@@ -1,43 +1,79 @@
 const canvas = document.querySelector(".gameConsole__screen");
 const controls = document.querySelectorAll(".gameConsole__controls span")
-/**
- * @type {HTMLCanvasElement}
-*/
 const game = canvas.getContext("2d");
 
 let canvasSize;
 let elementSize;
+
+let gameLevel = 0;
+let routesNotPermitted = [];
+
 const playerPosition = {
     x: undefined,
     y: undefined
-}
+};
+
+const startingPlayerPosition = {
+    x: undefined,
+    y: undefined
+};
+
+const endOfTheRoad = {
+    x: undefined,
+    y: undefined
+};
+
 
 window.addEventListener("load", setCanvasSize);
 window.addEventListener("resize", setCanvasSize);
 
 function startGame() {
-    game.font = elementSize + "px  Verdana"
-    game.textAlign ="start"
-    const map = maps[0]
-    const  rowsMaps = map.trim().split("\n")
-    const  mapRowsColums = rowsMaps.map((row)=>row.trim().split(''))
+    game.font = elementSize + "px  Verdana";
+    game.textAlign ="start";
+    const map = maps[gameLevel];
+    if (!map) {
+        console.log("hola");
+        gameWin()
+    }
+    const  rowsMaps = map.trim().split("\n");
+    const  mapRowsColums = rowsMaps.map((row)=>row.trim().split(''));
+
+    console.log({playerPosition,endOfTheRoad});
+    routesNotPermitted = [];
     game.clearRect(0,0,canvasSize,canvasSize)
     mapRowsColums.forEach((row,rowIndex)=>{
         row.forEach((column,columnIndex)=>{
-            const positionX = elementSize*columnIndex;
-            const positionY = elementSize*(rowIndex +1)
+            const positionX = Number((elementSize*columnIndex).toFixed());
+            const positionY = Number((elementSize*(rowIndex +1)).toFixed())
             if(column == "O" && !playerPosition.x && !playerPosition.y ){
                 playerPosition.x = positionX
                 playerPosition.y = positionY
+                startingPlayerPosition.x = positionX
+                startingPlayerPosition.y = positionY
+            }if(column == "I"){
+                endOfTheRoad.x = positionX
+                endOfTheRoad.y = positionY
+            }if (column == "X"){
+                routesNotPermitted.push({x:positionX,y:positionY})
             }
             game.fillText(emojis[column],positionX,positionY)
         })
     })
-    // PLAYER
+
     movePlayer()
 };
 function movePlayer() {
-    game.fillText(emojis["PLAYER"],playerPosition.x+(elementSize/8),playerPosition.y)
+    console.log(playerPosition.y);
+    const endOfTheRoadX = playerPosition.x.toFixed() == endOfTheRoad.x.toFixed()
+    const endOfTheRoadY = playerPosition.y.toFixed() == endOfTheRoad.y.toFixed()
+    const encollitionFound = endOfTheRoadX && endOfTheRoadY
+    if (encollitionFound) {
+        console.log("subiste de nivel");
+        levelWin()
+    }
+    validatedRouteposition()
+    game.fillText(emojis["PLAYER"],playerPosition.x,playerPosition.y)
+
 }
 function setCanvasSize() {
     function calculateCanvasSize(porcentaje) {
@@ -59,7 +95,7 @@ function setCanvasSize() {
     }else{
         calculateCanvasSize(0.3)
     }
-    elementSize = canvasSize / 10.4
+    elementSize = Number((canvasSize / 10.4).toFixed(1))
     startGame()
 };
 const movements ={
@@ -72,30 +108,45 @@ const movements ={
     s:moveDown,
     d:moveRight
 }
+function routeValidation() {
+    return routesNotPermitted.some(coordinates=>coordinates.x.toFixed()  == playerPosition.x.toFixed()  && coordinates.y.toFixed() == playerPosition.y.toFixed() )
+}
+function validatedRouteposition() {
+    console.log(routeValidation());
+    if (routeValidation()) {
+        game.fillText(emojis["BOMB_COLLISION"],playerPosition.x,playerPosition.y)
+        console.log(startingPlayerPosition);
+    }
+}
+function levelWin() {
+    gameLevel++
+    startGame()
+}
+function gameWin() {
+    console.log("terminaste el juego");
+}
 function moveUp() {
-
     if(playerPosition.y > elementSize){
         playerPosition.y -= elementSize
         startGame()
     }
 }
 function moveLeft() {
-    if( playerPosition.x > elementSize){
+    if( playerPosition.x > 0){
         playerPosition.x -= elementSize
         startGame()
     }
 }
 function moveRight() {
-    if(playerPosition.x < elementSize*9 ){
+    if(playerPosition.x < canvasSize-(2*elementSize) ){
         playerPosition.x += elementSize
         startGame()
+
     }
 }
 function moveDown() {
-    if(playerPosition.y < elementSize*10){
-        console.log("bottom");
+    if(playerPosition.y < canvasSize-elementSize){
         playerPosition.y += elementSize
-        console.log({playerPosition,elementSize})
         startGame()
     }
 }
